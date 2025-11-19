f5bff03ba0a10f0857a96aa12fa6ac61f35ac561
Isaac Barbosa <isaaccezar22@gmail.com>
2025-11-19 19:40:48 +0000

    first commit and adds sum_arrays ruby kata

---
+++ ruby/sum_array.rb
@@ -0,0 +1,24 @@
+=begin
+  Write a function that takes an array of numbers and returns the sum of the numbers.
+  The numbers can be negative or non-integer. If the array does not contain any numbers
+  than you should return 0.
+=end
+
+def sum(numbers)
+  numbers.reduce(0, :+)
+end
+
+# Test suite
+
+def dotest(numbers, expected)
+  it "numbers = #{numbers}" do
+    expect(sum(numbers)).to be_within(1e-5).of expected
+  end
+end
+
+describe "Fixed tests" do
+  dotest([1, 5.2, 4, 0, -1], 9.2)
+  dotest([], 0)
+  dotest([-2.398], -2.398)
+  dotest([0, -1, -2], -3)
+end
